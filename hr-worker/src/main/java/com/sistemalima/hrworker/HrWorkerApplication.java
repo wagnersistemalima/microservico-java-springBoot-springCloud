cd3883e11ef2d58a3aec6aa58978ab173ee940cc
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-12-05 16:36:23 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/sistemalima/hrworker/HrWorkerApplication.java
@@ -0,0 +1,15 @@
+package com.sistemalima.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+	
+	// comentario
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}

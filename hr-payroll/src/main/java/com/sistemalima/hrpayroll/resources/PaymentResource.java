6d5178abdbfabd46bfdd58bce53b53ca4e62daa1
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-12-15 19:23:32 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/sistemalima/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/sistemalima/hrpayroll/resources/PaymentResource.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sistemalima.hrpayroll.entities.Payment;
 import com.sistemalima.hrpayroll.services.PaymentService;
 
-// controlodar Rest
+// controlodor Rest
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -23,9 +24,17 @@ public class PaymentResource {
 	
 	// 1º end point / buscar pagamento por id
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok().body(payment);
+	}
+	
+	// metodo alternativo
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok().body(payment);
 	}
 	
